b84b1d658adcb15ab503925ed6bb9763a9da688a
Rafael Albino <ansdles@live.com>
2026-06-16 12:54:28 -0300

    fix: corrigido nome da função principal

--- src/screens/PokemonCreator/PokemonCreator.tsx
+++ src/screens/PokemonCreator/PokemonCreator.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { View, Text } from 'react-native';
 import { style } from './StylePokemonCreator';
 
-export default function PokedexCreator() {
+export default function PokemonCreator() {
     return (
     <View style={style.container}>
         <Text style={style.title}>Pokemon Creator Screen</Text>
